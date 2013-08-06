fd8a31dc5323c25786049ab85daa6f13d556e8c9
Francisco A. Lozano <francisco.lozano@kii.com>
2013-08-06 19:54:31 +0200

    Initial commit

---
+++ fabfile.py
@@ -0,0 +1,70 @@
+import logging
+from fabric.decorators import task
+from fabric.operations import *
+from fabric.contrib import *
+from xml.sax.saxutils import escape
+logging.basicConfig(level=logging.INFO)
+
+DEFAULT_APP='ufe'
+DEFAULT_INSTANCE='0'
+
+@task
+def build_filter():
+	###local("mvn clean package")
+	pass
+
+@task
+def upload_filter(remote_path):
+	files=put(local_path="./target/add-sleep-to-request*.jar", remote_path=remote_path, use_sudo=True, mode="644")
+	# TODO ensure only one JAR is built
+	return files[0]
+
+def _web_xml(app=DEFAULT_APP, instance=DEFAULT_INSTANCE):
+	return "/opt/%s/instances/%s/conf/web.xml" % (app, instance)
+
+def _prefix(name, pattern, min_sleep, max_sleep):
+	return "<!--sleep:name=%s,pattern=%s,min-sleep=%s,max-sleep=%s-->" % (name, escape(pattern), min_sleep, max_sleep)
+
+def _sed_escape(text):
+    for char in "/'().*":
+        text = text.replace(char, r'\%s' % char)
+
+    return text
+
+@task
+def build_content(name, pattern, min_sleep, max_sleep):
+	return "<filter><filter-name>%s</filter-name><filter-class>com.kii.common.SleepFilter</filter-class><init-param><param-name>pattern</param-name><param-value>%s</param-value></init-param><init-param><param-name>min-sleep</param-name><param-value>%s</param-value></init-param><init-param><param-name>max-sleep</param-name><param-value>%s</param-value></init-param></filter><filter-mapping><filter-name>%s</filter-name><url-pattern>/*</url-pattern></filter-mapping><!-- keep this in a single line for automation -->" % (name, pattern, min_sleep, max_sleep, name )
+
+@task
+def add_line_to_file(prefix, line,file,marker = "  <!-- ==================== Built In Filter Mappings ====================== -->"):
+	line_with_prefix = prefix+line
+	logging.info("Adding %s to %s after %s" % (line_with_prefix, file, marker))
+	#### REALLY HACKY
+	sudo("sed -i.bak -r -e '/%s/ a\%s' \"$(echo %s)\"" % (_sed_escape(marker), _sed_escape(line_with_prefix), file))
+
+@task
+def remove_line_from_file(contains, file):
+	logging.info("Removing %s from %s " % (contains, file))
+	sudo("sed -i.bak -r -e '/%s/d' \"$(echo %s)\"" % (_sed_escape(contains), file))
+
+@task
+def add_waits(name, pattern, min_sleep, max_sleep, app=DEFAULT_APP, instance=DEFAULT_INSTANCE):
+	build_filter()
+	jar_file = upload_filter(remote_path="/opt/%s/instances/%s/lib" % (app, instance))
+	line = build_content(name, pattern, min_sleep, max_sleep)
+	prefix = _prefix(name, pattern, min_sleep, max_sleep)
+	web_xml_file = _web_xml(app, instance)
+	add_line_to_file(prefix, line, web_xml_file)
+
+@task
+def list_waits(app=DEFAULT_APP, instance=DEFAULT_INSTANCE):
+	web_xml_file = _web_xml(app, instance)
+	sudo("grep \"%s\" \"%s\" " % ("sleep", web_xml_file ))
+
+
+@task
+def remove_waits(name, pattern, min_sleep, max_sleep, app=DEFAULT_APP, instance=DEFAULT_INSTANCE):
+	web_xml_file = _web_xml(app, instance)
+	prefix = _prefix(name, pattern, min_sleep, max_sleep)
+	web_xml_file = _web_xml(app, instance)
+	remove_line_from_file(prefix, web_xml_file)
---
+++ src/main/java/com/kii/common/SleepFilter.java
@@ -0,0 +1,75 @@
+package com.kii.common;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+public class SleepFilter implements Filter {
+
+	private static final Logger LOGGER = Logger.getLogger(SleepFilter.class
+			.getName());
+
+	private final Random random = new Random();
+
+	private Pattern requestPattern;
+
+	private int minSleepTime = 0;
+
+	private int maxSleepTime = 0;
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+		requestPattern = Pattern.compile(filterConfig
+				.getInitParameter("pattern"));
+		minSleepTime = Integer.parseInt(filterConfig
+				.getInitParameter("min-sleep"));
+		maxSleepTime = Integer.parseInt(filterConfig
+				.getInitParameter("max-sleep"));
+		LOGGER.warning("DON'T USE THIS SERVLET-FILTER IN PRODUCTION");
+		LOGGER.info("Initialized: pattern = " + requestPattern
+				+ ", minSleepTime=" + minSleepTime + ", maxSleepTime="
+				+ maxSleepTime);
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		if (isMatch(httpRequest)) {
+			sleep();
+		}
+	}
+
+	private void sleep() {
+		try {
+			int msecs = random.nextInt(maxSleepTime - minSleepTime)
+					+ minSleepTime;
+			LOGGER.info("Waiting " + msecs + " msecs");
+			Thread.sleep(msecs);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private boolean isMatch(HttpServletRequest request) {
+		Matcher m = requestPattern.matcher(request.getRequestURI());
+		if (m.matches()) {
+			LOGGER.info("Request matches: " + request.getRequestURI());
+			return true;
+		}
+		return false;
+	}
+
+	public void destroy() {
+
+	}
+
+}
